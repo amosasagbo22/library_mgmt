99d66227ea5793861cc4d374cc207ca78fffb7c5
amosasagbo22 <107491582+amosasagbo22@users.noreply.github.com>
2025-12-15 05:37:57 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 5000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// MongoDB Connection
+mongoose.connect('mongodb+srv://aasagbo:JzaVNLotgEFNq8VZ@librarydb.mq8hcy8.mongodb.net/?retryWrites=true&w=majority&appName=libraryDB', {
+    appName: 'libraryDB'
+})
+    .then(() => console.log('Connected to MongoDB Atlas!'))
+    .catch(err => console.error('Error connecting to MongoDB Atlas:', err));
+
+// Import Routes
+const bookRoutes = require('./src/library-api/routes/bookRoutes');
+const memberRoutes = require('./src/library-api/routes/memberRoutes');
+const staffRoutes = require('./src/library-api/routes/staffRoutes');
+// API Routes
+app.use('/api/books', bookRoutes);
+app.use('/api/members', memberRoutes);
+app.use('/api/staffs', staffRoutes);
+
+// Start Server
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
